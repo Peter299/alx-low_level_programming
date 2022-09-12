0c97752477082c6d9695fe7f99ab1012aebf47c0
Peter299 <robertspeter299@gmail.com>
2022-09-11 17:48:38 -0700

    correction

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,28 @@
+#include <studio.h>
+
+/**
+* main - print the string in the put function
+*
+* Description: using the main function
+* this program prints "Programming is like building a multilingual puzzle
+* Return: 0
+*/
+int main(void)
+{
+        puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
+^C
+
+^c
+q
+
+
+
+
+
+
+^c
+^C
+q
+Q
